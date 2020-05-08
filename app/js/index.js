dabec5de2724ce35b5c2e1b8bf979e2f8d3efde5
Shawn Gettler <shawn.gettler@gmail.com>
2020-05-08 17:31:47 -0400

    added command line interpreter

--- app/js/index.js
+++ app/js/index.js
@@ -3,7 +3,7 @@
 /**
  * index.js
  * @author @shawngettler
- * 
+ *
  * Utility for Skytraq-based GPS logger.
  */
 
@@ -13,19 +13,32 @@
 
 // npm modules
 import SerialPort from "serialport";
+import Commander from "commander";
 
 
 
-console.log("Detecting serial devices...");
-SerialPort.list().then(function(ports) {
-    if(ports.length > 0) {
-        console.log("Found device on port(s):");
-        for(let p of ports) {
-            console.log(p.path+" "+p.manufacturer+" "+p.vendorId+" "+p.productId);
+/*
+ * Parse command line options.
+ */
+const command = new Commander.Command();
+command.option("-l, --list-devices", "list serial devices");
+command.parse(process.argv);
+
+
+/*
+ * List connected serial devices.
+ */
+if(command.listDevices) {
+    console.log("Detecting serial devices...");
+    SerialPort.list().then(function(ports) {
+        if(ports.length > 0) {
+            console.log("Found device on port(s):");
+            for(let p of ports) {
+                console.log(p.path);
+            }
+        } else {
+            console.log("No devices detected.");
         }
-    } else {
-        console.log("No devices detected.");
-    }
-});
-
-
+        process.exit(0);
+    });
+}
