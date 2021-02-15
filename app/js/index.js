0122d72ce49d8253a456ad89104d7221fefacfc9
Shawn Gettler <shawn.gettler@gmail.com>
2021-02-15 12:25:53 -0500

    added command to connect to device

--- app/js/index.js
+++ app/js/index.js
@@ -22,6 +22,8 @@ import Commander from "commander";
  */
 const command = new Commander.Command();
 command.option("-l, --list-devices", "list serial devices");
+command.arguments("[port]");
+command.action((port) => { command.port = port; });
 command.parse(process.argv);
 
 
@@ -30,15 +32,39 @@ command.parse(process.argv);
  */
 if(command.listDevices) {
     console.log("Detecting serial devices...");
-    SerialPort.list().then(function(ports) {
-        if(ports.length > 0) {
-            console.log("Found device on port(s):");
-            for(let p of ports) {
-                console.log(p.path);
+    SerialPort.list()
+        .then((ports) => {
+            if(ports.length > 0) {
+                console.log("Found device on port(s):");
+                for(let p of ports)
+                    console.log(p.path);
+            } else {
+                console.log("No devices detected.");
             }
-        } else {
-            console.log("No devices detected.");
-        }
-        process.exit(0);
-    });
+            process.exit(0);
+        });
+}
+
+
+/*
+ * Connect to the device and run utility.
+ */
+if(command.port) {
+    console.log("Connecting to device on port "+command.port);
+    const serial = new SerialPort(command.port, { autoOpen: false });
+    serial.on("error", (err) => { console.log(err); });
+
+    // wrap serianport in promises
+    const serialOpen = () => {
+        return new Promise((resolve, reject) => {
+            serial.open(resolve);
+        });
+    };
+    const serialWrite = (m) => {
+        return new Promise((resolve, reject) => {
+            serial.write(m);
+            serial.drain(resolve);
+        });
+    };
+
 }
